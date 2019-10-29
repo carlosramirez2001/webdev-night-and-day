1eb6f9bc184044670f99f594ec7dce03735af1ff
Carlos Ramirez <20477@columbushs.com>
2019-10-29 11:20:54 -0400

    started

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('button').click(function(){
+        if ($('body').hasClass('black')) {
+            $('body').removeClass('black').addClass('white');
+        }
+        else {
+            $('body').removeClass('white').addClass('black');
+        }
+    });
+});
+
